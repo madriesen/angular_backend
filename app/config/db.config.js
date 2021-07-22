876b9c5cba628ada169547eb1e0097c601d3fc40
Martijn Driesen <info@martijndriesen.me>
2021-07-22 13:30:29 +0200

    feat: move env variables into env file

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://localhost:27017/angular"
+  url: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 };
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = require("./app/models");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: process.env.CORS_ORIGIN
 };
 
 
