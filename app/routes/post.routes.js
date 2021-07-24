7ef27603d16eb8cca72c26b24c26b38bd0a2ebd2
Gust van der Sanden <gustvdsanden@gmail.com>
2021-07-24 15:02:46 +0200

    chore: made 1 route protected for testing

--- app/routes/post.routes.js
+++ app/routes/post.routes.js
@@ -1,13 +1,15 @@
+const { verifyToken } = require("../middlewares/authJwt");
+
 module.exports = app => {
   const posts = require("../controllers/post.controller.js");
 
   var router = require("express").Router();
-
+  const authorize = require("../middlewares/authJwt")
   // Create a new article
   router.post("/", posts.create);
 
   // Retrieve all articles
-  router.get("/", posts.findAll);
+  router.route("/").get(verifyToken, posts.findAll);
 
   app.use('/api/Post', router);
 };
