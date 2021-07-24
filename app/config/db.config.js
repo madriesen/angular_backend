30c8d47c752ae9beb8c92416ef9f1e37cfdf3814
Gust van der Sanden <gustvdsanden@gmail.com>
2021-07-24 13:34:39 +0200

    chore: makes heroku work with mongodb atlas db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,5 +2,5 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  url: `${process.env.DB_PROVIDER}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+  url: process.env.HEROKU_CONNECTION_STRING? process.env.HEROKU_CONNECTION_STRING : `${process.env.DB_PROVIDER}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 };
