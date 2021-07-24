11075115dceb9383220927f75a080dacbd4402c0
madriesen <martijn.driesen5@hotmail.com>
2021-07-24 11:02:47 +0200

    add db_provider to env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,5 +2,5 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  url: `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+  url: `${process.env.DB_PROVIDER}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 };
