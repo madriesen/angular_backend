a8b53f880e5e2ca88ae048b3200f010ce51feb57
madriesen <martijn.driesen5@hotmail.com>
2021-07-30 11:18:45 +0200

    add try catch around splitting the token

--- app/middlewares/authJwt.js
+++ app/middlewares/authJwt.js
@@ -6,7 +6,7 @@ const User = db.user;
 verifyToken = (req, res, next) => {
   let token = req.headers["x-access-token"];
 
-  if (!token) token = req.headers['authorization'].split(' ')[1]
+  if (!token) try {token = req.headers['authorization'].split(' ')[1]} catch(e) {}
 
   if (!token) {
     return res.status(403).send({ message: "No token provided!" });
