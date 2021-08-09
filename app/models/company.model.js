aababcc8aea3349fa05b47e19357adc03199327a
gustvdsanden <gustvdsanden@gmail.com>
2021-08-09 14:30:40 +0200

    chore: initial change to model

--- app/models/company.model.js
+++ app/models/company.model.js
@@ -1,9 +1,15 @@
+const { Schema } = require("mongoose");
+
 module.exports = mongoose => {
   const Company = mongoose.model(
     "Company",
     mongoose.Schema(
       {
-        Name: String
+        Name: String,
+        Address: String,
+        Description: String,
+        Owner:{type: Schema.Types.ObjectId, ref: 'User'},
+        Colleagues: [{type: Schema.Types.ObjectId, ref:'User'}],
       },
       { timestamps: true }
     )
