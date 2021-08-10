da211a97d1069208bcbe828d266a06cdd00fc1d0
madriesen <martijn.driesen5@hotmail.com>
2021-08-10 20:54:02 +0200

    on update user return updated user

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,15 +1,15 @@
-const config = require("../config/auth.config");
-const db = require("../models");
+const config = require('../config/auth.config');
+const db = require('../models');
 const User = db.users;
 
-var jwt = require("jsonwebtoken");
-var bcrypt = require("bcryptjs");
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
 
 // Create and Save a new user
 exports.create = (req, res) => {
   // Validate request
   if (!req.body.Email) {
-    res.status(400).send({ message: "Content can not be empty!" });
+    res.status(400).send({ message: 'Content can not be empty!' });
     return;
   }
 
@@ -20,81 +20,72 @@ exports.create = (req, res) => {
     Email: req.body.Email,
     Username: req.body.Username ? req.body.Username : req.body.Email,
     Password: req.body.Password,
-    RoleID: req.body.RoleID
+    RoleID: req.body.RoleID,
   });
 
   // Save user in the database
   user
     .save(user)
-    .then(data => {
+    .then((data) => {
       res.send(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the user."
+        message: err.message || 'Some error occurred while creating the user.',
       });
     });
 };
-
 
 // Find a single user with an id
 exports.findOne = (req, res) => {
   const id = req.params.id;
 
-  User.findById(id).populate('RoleID').populate('Company')
-    .then(data => {
-      if (!data)
-        res.status(404).send({ message: "Not found user with id " + id });
+  User.findById(id)
+    .populate('RoleID')
+    .populate('Company')
+    .then((data) => {
+      if (!data) res.status(404).send({ message: 'Not found user with id ' + id });
       else res.send(data);
     })
-    .catch(err => {
-      res
-        .status(500)
-        .send({ message: "Error retrieving user with id=" + id });
+    .catch((err) => {
+      res.status(500).send({ message: 'Error retrieving user with id=' + id });
     });
 };
 
-
 exports.authenticate = (req, res) => {
-
   User.findOne({
-    Username: req.body.Username
-  })
-    .exec((err, user) => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
+    Username: req.body.Username,
+  }).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
 
-      if (!user) {
-        return res.status(404).send({ message: "User Not found." });
-      }
+    if (!user) {
+      return res.status(404).send({ message: 'User Not found.' });
+    }
 
-      var passwordIsValid = bcrypt.compareSync(
-        req.body.Password,
-        user.Password
-      );
+    var passwordIsValid = bcrypt.compareSync(req.body.Password, user.Password);
 
-      if(req.body.Password == user.Password){
-        passwordIsValid = true;
-      }
+    if (req.body.Password == user.Password) {
+      passwordIsValid = true;
+    }
 
-      if (!passwordIsValid) {
-        return res.status(401).send({
-          accessToken: null,
-          message: "Invalid Password!"
-        });
-      }
-      var token = jwt.sign({ id: user.id, CompanyId:user.Company? user.Company:'' }, config.secret, {
-        expiresIn: 86400 // 24 hours
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: 'Invalid Password!',
       });
-      res.status(200).send({
-        _id: user._id,
-        AccessToken: token,
-        CompanyId:user.Company? user.Company:''
-      });
+    }
+    var token = jwt.sign({ id: user.id, CompanyId: user.Company ? user.Company : '' }, config.secret, {
+      expiresIn: 86400, // 24 hours
     });
+    res.status(200).send({
+      _id: user._id,
+      AccessToken: token,
+      CompanyId: user.Company ? user.Company : '',
+    });
+  });
 };
 
 // Delete a user with the specified id in the request
@@ -102,20 +93,20 @@ exports.delete = (req, res) => {
   const id = req.params.id;
 
   User.findByIdAndRemove(id)
-    .then(data => {
+    .then((data) => {
       if (!data) {
         res.status(404).send({
-          message: `Cannot delete user with id=${id}. Maybe user was not found!`
+          message: `Cannot delete user with id=${id}. Maybe user was not found!`,
         });
       } else {
         res.send({
-          message: "user was deleted successfully!"
+          message: 'user was deleted successfully!',
         });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Could not delete user with id=" + id
+        message: 'Could not delete user with id=' + id,
       });
     });
 };
@@ -123,23 +114,23 @@ exports.delete = (req, res) => {
 exports.update = (req, res) => {
   if (!req.body) {
     return res.status(400).send({
-      message: "Data to update can not be empty!"
+      message: 'Data to update can not be empty!',
     });
   }
 
   const id = req.params.id;
 
   User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-    .then(data => {
-      if (!data) {
+    .then((user) => {
+      if (!user) {
         res.status(404).send({
-          message: `Cannot update user with id=${id}. Maybe user was not found!`
+          message: `Cannot update user with id=${id}. Maybe user was not found!`,
         });
-      } else res.send({ message: "user was updated successfully." });
+      } else res.send(user);
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).send({
-        message: "Error updating user with id=" + id
+        message: 'Error updating user with id=' + id,
       });
     });
 };
